faef3fbb5e7a541acbdc3276a2380845f7cc0648
anylerolero <anyruizd@gmail.com>
2017-07-16 20:48:47 -0500

    corrección de unos errores raros

--- blog/views.py
+++ blog/views.py
@@ -15,14 +15,15 @@ def post_detail(request, pk):
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.published_date = timezone.now()
-        post.save()
-    return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog/post_edit.html', {'form': form})
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
